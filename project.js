01f77dfd8b24d400fbdfa868c13105897562e5c0
Mary West <westmarye1219@gmail.com>
2018-10-31 18:47:18 -0500

    updated files

--- project.js
+++ project.js
@@ -1,24 +1,4 @@
-let projects = [
-{
-    title: "Cool Project",
-    screenshot: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSDlPgWaDMQ-uYmWtoayU1dTHfAX2OrVtE5sTtWrG908-QPjACfA", 
-    description: "This is the best project",// A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-  },
-{
-    title: "Cool Project", 
-    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
-    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
-    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
-}
 
-];
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
@@ -68,9 +48,6 @@ const techEvent = () => {
         document.getElementById('technologiesPage').classList.remove('hideStuff')
     })
 };
-
-
-
 
 
 
