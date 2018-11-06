a641670929aa38347deebd584f28fe66ddaa4bc0
Mary West <westmarye1219@gmail.com>
2018-11-05 21:18:16 -0600

    2/3 of navbar works

--- javascripts/components/project.js
+++ javascripts/components/project.js
@@ -1,69 +1,29 @@
 import $ from 'jquery';
+import projectsData from '../data/projectsData';
 
-const createProjectCards = () => {
-    let newString = '';
-    for(let i=0; i<projects.length; i+=1){
-        newString += `<div class ="statement">`;
-        newString += `<h6>${projects[i].title}</h6>`;
-        newString += `<img>${projects[i].screenshot}</img>`;
-        newString += `<h6>${projects[i].description}</h6>`;
-        newString += `<h6>${projects[i].technologiesUsed}</h6>`;
-        newString += `<h6>${projects[i].available}</h6>`;
-        newString += `<a href=${projects[i].url}>Click here</a>`;
-        newString += `<h6>${projects[i].githubUrl}</h6>`;
-        newString += `</div>`;
-        if (projects[i].available === true) {
-        $('#projectsPage').html(newString);
-        }
-    }        
+const createProjectCards = (projects) => {
+  let newString = '';
+  for (let i = 0; i < projects.length; i += 1) {
+    newString += '<div class ="statement">';
+    newString += `<h6>${projects[i].title}</h6>`;
+    newString += `<img>${projects[i].screenshot}</img>`;
+    newString += `<h6>${projects[i].description}</h6>`;
+    newString += `<h6>${projects[i].technologiesUsed}</h6>`;
+    newString += `<h6>${projects[i].available}</h6>`;
+    newString += `<a href=${projects[i].url}>Click here</a>`;
+    newString += `<h6>${projects[i].githubUrl}</h6>`;
+    newString += '</div>';
+    if (projects[i].available === true) {
+      $('#projectsPage').html(newString);
+    }
+  }
 };
-createProjectCards();
-
-// const bioEvent = () => {
-//     document.getElementById('navToBio').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('technologiesPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.add('hideStuff')
-//         document.getElementById('bioPage').classList.remove('hideStuff')
-//     })
-// };
-// const projectEvent = () => {
-//     document.getElementById('navToProjects').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('technologiesPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.remove('hideStuff')
-//         document.getElementById('bioPage').classList.add('hideStuff')
-//     })
-// };
-// const techEvent = () => {
-//     document.getElementById('navToTechnologies').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('bioPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.add('hideStuff')
-//         document.getElementById('technologiesPage').classList.remove('hideStuff')
-//     })
-// };
-
-
-
-// const addEvents = () => {
-//     bioEvent();
-//     projectEvent();
-//     techEvent();
-// };
-
-// addEvents();
 
 const initializeProjectsView = () => {
-    movieData.loadProjects().then((projects) => {
+  projectsData.loadProjects().then((projects) => {
     createProjectCards(projects);
-    }).catch((error) => {
-      console.error(error);
-    });
-   };
-export default createProjectCards;
-
-
-
-
-
+  }).catch((error) => {
+    console.error(error);
+  });
+};
+export default initializeProjectsView;
--- javascripts/helpers/events.js
+++ javascripts/helpers/events.js
@@ -1,80 +1,38 @@
 import $ from 'jquery';
 import 'bootstrap';
-import './index.scss';
-import {createProjectCards} from './projects.js'
+
+import initializeProjectsView from '../components/project';
 
 const projectEvent = () => {
-    $('#navToBio').on('click',(event)=>{
-        $('#technologiesPage').hide()
-        $('#projectsPage').show()
-        $('#bioPage').hide()
-    })
-}
-
-const bioEvent = () => {
-    $('#navToProjects').on('click',(event)=>{
-        $('#technologiesPage').hide()
-        $('#projectsPage').hide()
-        $('#bioPage').show()
-    })
-}
-const techEvent = () => {
-    $('#navToTechnologies').on('click',(event)=>{
-        $('#technologiesPage').show()
-        $('#projectsPage').hide()
-        $('#bioPage').hide()
-    })
-}
-// const bioEvent = () => {
-//     $('#navToProjects').on('click',()=>{
-//         $('#technologiesPage').closest('fullPage').hide()
-//         $('#projectsPage').closest('fullPage').hide()
-//         $('#bioPage').closest('fullPage').show()
-//     })
-// }
-const addEvents = () => {
-    bioEvent();
-    projectEvent();
-    techEvent();
-    createProjectCards();
-
+  $('#navToProjects').on('click', () => {
+    $('#technologiesPage').hide();
+    $('#projectsPage').show();
+    $('#bioPage').hide();
+  });
 };
 
-addEvents()
+const bioEvent = () => {
+  $('#navToBio').on('click', () => {
+    $('#technologiesPage').hide();
+    $('#projectsPage').hide();
+    $('#bioPage').show();
+  });
+};
+const techEvent = () => {
+  $('#navToTechnologies').on('click', () => {
+    $('#technologiesPage').show();
+    $('#projectsPage').hide();
+    $('#bioPage').hide();
+  });
+};
 
-// const bioEvent = () => {
-//     document.getElementById('navToBio').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('technologiesPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.add('hideStuff')
-//         document.getElementById('bioPage').classList.remove('hideStuff')
-//     })
-// };
-// const projectEvent = () => {
-//     document.getElementById('navToProjects').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('technologiesPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.remove('hideStuff')
-//         document.getElementById('bioPage').classList.add('hideStuff')
-//     })
-// };
-// const techEvent = () => {
-//     document.getElementById('navToTechnologies').addEventListener('click', function(event) {
-//         event.preventDefault()
-//         document.getElementById('bioPage').classList.add('hideStuff')
-//         document.getElementById('projectsPage').classList.add('hideStuff')
-//         document.getElementById('technologiesPage').classList.remove('hideStuff')
-//     })
-// };
+const addEvents = () => {
+  bioEvent();
+  projectEvent();
+  techEvent();
+  initializeProjectsView();
+};
 
-
-
-// const addEvents = () => {
-//     bioEvent();
-//     projectEvent();
-//     techEvent();
-// };
-
-// addEvents();
+addEvents();
 
 export default addEvents;
